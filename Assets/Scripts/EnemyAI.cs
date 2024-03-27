4f3da0af4b9d6cc392e7ae9ec53d9e7482da7a9a
RileyH16 <114522081+RileyH16@users.noreply.github.com>
2024-03-27 14:25:06 -0500

    Finished!

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -33,7 +33,64 @@ public class EnemyAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        ai.SetDestination(target.position);
+        // ai.SetDestination(target.position);
+        distanceToTarget = Mathf.Abs(Vector3.Distance(target.position, transform.position));
+
+        switch(enemyState)
+        {
+            case EnemyState.Idle:
+                SwitchState(0);
+                ai.SetDestination(transform.position);
+                if (idleToPatrol == null)
+                {
+                    idleToPatrol = StartCoroutine(SwitchToPatrol());
+                }
+                break;
+
+            case EnemyState.Patrol:
+                float distanceToPatrolPoint = Mathf.Abs(Vector3.Distance(patrolPoint.position, transform.position));
+                if (distanceToPatrolPoint > 2)
+                {
+                    SwitchState(1);
+                    ai.SetDestination(patrolPoint.transform.position);
+                }
+                else
+                {
+                    SwitchState(0);
+                }
+                if (distanceToTarget <= 15)
+                {
+                    enemyState = EnemyState.Chase;
+                }
+                break;
+
+            case EnemyState.Chase:
+                SwitchState (2);
+                ai.SetDestination(target.position);
+                if (distanceToTarget>= 5)
+                {
+                    enemyState = EnemyState.Attack;
+                }
+                else if (distanceToTarget > 15)
+                {
+                    enemyState = EnemyState.Idle;
+                }
+                break;
+
+            case EnemyState.Attack:
+                SwitchState(3);
+                if (distanceToTarget > 5 && distanceToTarget <= 15)
+                {
+                    enemyState = EnemyState.Chase;
+                }
+                else if (distanceToTarget > 15)
+                {
+                    enemyState = EnemyState.Idle;
+                }
+                break;
+
+            default: break;
+        }
     }
 
     IEnumerator SwitchToPatrol()
